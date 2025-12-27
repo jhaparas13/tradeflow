51c6a680e2d0305e0e11f77005c30b952b94d0a6
Paras <jhaparas13@gmail.com>
2025-12-27 22:49:06 +0530

    Add JwtService Class

---
+++ src/main/java/com/paras/tradeflow/security/jwt/JwtService.java
@@ -0,0 +1,58 @@
+package com.paras.tradeflow.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private static String SECRET_KEY;
+
+    @Value("${jwt.expiration}")
+    private static long EXPIRATION_TIME;
+
+    private SecretKey getSigningKey(){
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generateToken(String email){
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
+                .signWith(getSigningKey())
+                .compact();
+
+    }
+
+    private Claims extractClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractEmail(String token){
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, String email)
+    {
+        return extractEmail(token).equals(email) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token)
+    {
+        return extractClaims(token)
+                .getExpiration()
+                .before(new Date());
+    }
+}
