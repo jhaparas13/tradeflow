66596f0aa6f7e12a84748694877400ca2d165589
Paras <jhaparas13@gmail.com>
2025-12-29 23:45:14 +0530

    Add RefreshToken Entity and it's JPA repo

---
+++ src/main/java/com/paras/tradeflow/entity/RefreshToken.java
@@ -0,0 +1,21 @@
+package com.paras.tradeflow.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    private User user;
+
+    private Instant expiryDate;
+}
