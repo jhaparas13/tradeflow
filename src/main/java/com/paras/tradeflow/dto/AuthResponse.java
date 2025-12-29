70d7a2579e61cf546c7a333168343b12a5915d97
Paras <jhaparas13@gmail.com>
2025-12-29 22:47:41 +0530

    Add AuthResponse DTO Class

---
+++ src/main/java/com/paras/tradeflow/dto/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.paras.tradeflow.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+    private String accessToken;
+    private UserResponse userResponse;
+}
