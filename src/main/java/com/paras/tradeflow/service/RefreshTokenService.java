65fea1ae26a0b0dbc60fc14b0c7231ec0432bffb
Paras <jhaparas13@gmail.com>
2026-01-01 12:52:48 +0530

    Implement refresh token rotation and reuse detection

--- src/main/java/com/paras/tradeflow/service/RefreshTokenService.java
+++ src/main/java/com/paras/tradeflow/service/RefreshTokenService.java
@@ -3,34 +3,51 @@ package com.paras.tradeflow.service;
 import com.paras.tradeflow.entity.RefreshToken;
 import com.paras.tradeflow.entity.User;
 import com.paras.tradeflow.repository.RefreshTokenRepository;
+import com.paras.tradeflow.repository.UserRepository;
+import com.paras.tradeflow.security.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class RefreshTokenService {
 
-    private final RefreshTokenRepository repo;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
 
     public RefreshToken create(User user){
+
+        refreshTokenRepository.deleteByUser(user);
+
         RefreshToken token = new RefreshToken();
         token.setUser(user);
         token.setToken(UUID.randomUUID().toString());
         token.setExpiryDate(Instant.now().plus(30, ChronoUnit.DAYS));
-        return repo.save(token);
+        return refreshTokenRepository.save(token);
     }
 
     public RefreshToken verify(String token){
-        RefreshToken refreshToken = repo.findByToken(token).
-                orElseThrow(()-> new RuntimeException("Invalid refresh token"));
 
+        Optional<RefreshToken> refreshTokenCheck = refreshTokenRepository.findByToken(token);
+
+        if (refreshTokenCheck.isEmpty()){
+            String email = jwtService.extractEmail(token);
+            User user = userRepository.findByEmail(email).
+                    orElseThrow();
+
+            refreshTokenRepository.deleteByUser(user);
+            throw new RuntimeException("Refresh Token Reuse Detected");
+        }
+        RefreshToken refreshToken = refreshTokenCheck.get();
         if(refreshToken.getExpiryDate().isBefore(Instant.now()))
-            throw new RuntimeException("Refresh token expird");
-
-        return refreshToken;
+            throw new RuntimeException("Refresh token expired");
+        refreshTokenRepository.delete(refreshToken);
+        return create(refreshToken.getUser());
     }
 }
